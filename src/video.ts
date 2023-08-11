fee2d2d959630e0468da99adf0d1dec1bd1e9bad
nino-porcino <nino.porcino@gmail.com>
2023-08-11 20:25:51 +0200

    new canvas drawing method, double pixels from C

--- src/video.ts
+++ src/video.ts
@@ -1,10 +1,11 @@
 import { get_wasm_instance } from "./emscripten_wrapper";
 
 // good values for CRT PAL
-let SCREEN_W = 392;
-let SCREEN_H = 272;
-let POS_X = 3*8;
-let POS_Y = 3*8;
+const SCREEN_W = 392;
+const SCREEN_H = 272;
+const POS_X = 3*8;
+const POS_Y = 3*8;
+const PIXEL_WIDTH = 2;
 
 /*
 // values for debug
@@ -19,8 +20,8 @@ let saturation = 1.0;
 export function calculateGeometry() {
    // canvas is the outer canvas where the aspect ratio is corrected
    let canvas = document.getElementById("canvas") as HTMLCanvasElement;
-   canvas.width  = SCREEN_W * 2 -(POS_X*2);
-   canvas.height = SCREEN_H * 2 -(POS_Y*2);
+   canvas.width  = SCREEN_W * PIXEL_WIDTH - (POS_X*PIXEL_WIDTH);
+   canvas.height = SCREEN_H * PIXEL_WIDTH - (POS_Y*PIXEL_WIDTH);
 }
 
 /**************************************************/
@@ -36,28 +37,34 @@ let HH = SCREEN_H;
 let vic_ii_canvas = document.getElementById("canvas") as HTMLCanvasElement;
 let vic_ii_context = vic_ii_canvas.getContext('2d');
 
-// new method
+
+export function vdp_screen_update(ptr: number) {
+   /*
+   // 1 pixel wide version
+   let size1 = WW*HH*4;
+   const heap = get_wasm_instance().HEAPU8.subarray(ptr,ptr+size1);
+   const data = new Uint8ClampedArray(heap);       
+   const img = new ImageData(data, WW, HH);
+   vic_ii_context!.putImageData(img, 0, 0);   
+   return;
+   */
+     
+   // 2 pixel wide version
+   let size1 = WW*HH*4*PIXEL_WIDTH*PIXEL_WIDTH;
+   const heap = get_wasm_instance().HEAPU8.subarray(ptr,ptr+size1);
+   const data = new Uint8ClampedArray(heap);       
+   const img = new ImageData(data, WW*PIXEL_WIDTH, HH*PIXEL_WIDTH);
+   vic_ii_context!.putImageData(img, -POS_X, -POS_Y);   
+}
+
+/*
+
+// old drawing method
+
 let vic_ii_imageData = vic_ii_context!.createImageData(WW*2, HH*2);
 let bmp = new Uint32Array(vic_ii_imageData.data.buffer);
 
-/*
-// old drawing method
-let vic_ii_imageData = vic_ii_context.getImageData(0, 0, WW*2, HH*2);
-let imagedata_buffer = new ArrayBuffer(vic_ii_imageData.data.length);
-let imagedata_buf8 = new Uint8ClampedArray(imagedata_buffer);
-let bmp = new Uint32Array(imagedata_buffer);
-*/
-
-/*
-// Get 2d drawing context
-const ctx = document.getElementById('c').getContext('2d');
-const pointer = instance.exports._render();
-const data = new Uint8ClampedArray(memory.buffer, pointer, width * height * 4);
-const img = new ImageData(data, width, height);
-ctx.putImageData(img, 0, 0);
-*/
-
-export function vdp_screen_update(ptr: number) {
+export function vdp_screen_update_old(ptr: number) {   
    let start = ptr / get_wasm_instance().HEAPU32.BYTES_PER_ELEMENT;
    let size = WW*HH;
    let buffer = get_wasm_instance().HEAPU32.subarray(start,start+size);   
@@ -78,17 +85,16 @@ export function vdp_screen_update(ptr: number) {
       ptr1 += WW*2;
       ptr2 += WW*2;
    }
-
-   /*
+   
    // old drawing method
-   vic_ii_imageData.data.set(imagedata_buf8);
-   vic_ii_context.putImageData(vic_ii_imageData, -POS_X, -POS_Y);
-   */
-
+   //vic_ii_imageData.data.set(imagedata_buf8);
+   //vic_ii_context.putImageData(vic_ii_imageData, -POS_X, -POS_Y);
+   
    // new method
    vic_ii_context!.putImageData(vic_ii_imageData, -POS_X, -POS_Y);
 
    //frames++;
    //if(end_of_frame_hook !== undefined) end_of_frame_hook();
 }
+*/
 
--- wasm/sys_c64.c
+++ wasm/sys_c64.c
@@ -31,54 +31,74 @@ typedef uint16_t word;
 c64_desc_t desc;
 c64_t sys;
 
-#define PIXBUFSIZE (641024) /*(392*272*4)*/
-unsigned char pixel_buffer[PIXBUFSIZE];
-
-uint8_t c64palette[16][3] = {
-    { 0x00,0x00,0x00 },
-    { 0xFF,0xFF,0xFF },
-    { 0x68,0x37,0x2B },
-    { 0x70,0xA4,0xB2 },
-    { 0x6F,0x3D,0x86 },
-    { 0x58,0x8D,0x43 },
-    { 0x35,0x28,0x79 },
-    { 0xB8,0xC7,0x6F },
-    { 0x6F,0x4F,0x25 },
-    { 0x43,0x39,0x00 },
-    { 0x9A,0x67,0x59 },
-    { 0x44,0x44,0x44 },
-    { 0x6C,0x6C,0x6C },
-    { 0x9A,0xD2,0x84 },
-    { 0x6C,0x5E,0xB5 },
-    { 0x95,0x95,0x95 }
-};
-
 #define AUDIOBUFSIZE (1024)
 
 void audio_cb(const float* samples, int num_samples, void* user_data) {
    byte unused = (byte) EM_ASM_INT({ audio_buf_ready($0, $1); }, samples, num_samples );
 }
 
+uint32_t c64palette_RGBA[16] = {
+    0xFF000000,
+    0xFFFFFFFF,
+    0xFF2B3768,
+    0xFFB2A470,
+    0xFF863D6F,
+    0xFF438D58,
+    0xFF792835,
+    0xFF6FC7B8,
+    0xFF254F6F,
+    0xFF003943,
+    0xFF59679A,
+    0xFF444444,
+    0xFF6C6C6C,
+    0xFF84D29A,
+    0xFFB55E6C,
+    0xFF959595
+};
+
+#define PIXEL_WIDTH_2 1
+
+#ifdef PIXEL_WIDTH_2
+uint32_t pixel_buffer[392*272*4];
 void end_frame_cb(void* user_data) {
    uint8_t *src = sys.vic.crt.fb;
-   uint8_t *dst = pixel_buffer;
-   uint8_t c64_pixel;
+   uint32_t *dst = pixel_buffer;
+   uint32_t c64_pixel;
 
    for(int r=0;r<272;r++) {
       for(int c=0;c<392;c++) { 
-         uint8_t *c64color = c64palette[*src];
-         *dst++ = c64color[0];
-         *dst++ = c64color[1];
-         *dst++ = c64color[2]; 
-         *dst++ = 255;         
+         c64_pixel = c64palette_RGBA[*src];
+         dst[0] = c64_pixel;
+         dst[1] = c64_pixel;
+         dst[392*2] = c64_pixel;
+         dst[392*2+1] = c64_pixel;
+         src++;    
+         dst+=2;
+      }   
+      src += (63*8)- 392;
+      dst += 392*2;
+   }
+   byte unused = (byte) EM_ASM_INT({ vdp_screen_update($0); }, pixel_buffer );
+}
+#else
+uint32_t pixel_buffer[392*272];
+void end_frame_cb(void* user_data) {
+   uint8_t *src = sys.vic.crt.fb;
+   uint32_t *dst = (uint32_t *)pixel_buffer;
+   uint32_t c64_pixel;
+
+   for(int r=0;r<272;r++) {
+      for(int c=0;c<392;c++) { 
+         c64_pixel = c64palette_RGBA[*src];
+         *dst++ = c64_pixel;
          src++;    
       }   
       src += (63*8)- 392;
    }
-#endif
-
    byte unused = (byte) EM_ASM_INT({ vdp_screen_update($0); }, pixel_buffer );
 }
+#endif
+
 
 EMSCRIPTEN_KEEPALIVE
 void sys_init() {
