6f0795050591e18908303e5bd022b0d243a712d6
nino-porcino <nino.porcino@gmail.com>
2023-08-13 15:54:09 +0200

    implement 6499 autoanswer

--- src/cbm_6499/cbm_6499.ts
+++ src/cbm_6499/cbm_6499.ts
@@ -3,6 +3,7 @@ import { M6821 } from "./m6821";
 import { M6850 } from "./m6850";
 
 import { BBSConnector } from "../bbs_connector";
+import { array2String } from "../utils";
 
 let PIA = new M6821();
 let ACIA = new M6850();
@@ -10,6 +11,8 @@ let ACIA = new M6850();
 export let videotel = new BBSConnector();
 
 (window as any).ACIA = ACIA;
+(window as any).PIA = PIA;
+(window as any).videotel = videotel;
 
 videotel.on_error = ()=>{
    // sets NO CARRIER
@@ -35,6 +38,8 @@ videotel.on_data = (data)=>{
 }
 
 ACIA.transmit_data = (data) => {
+   //console.log(array2String([data]));
+   //console.log(`0x${data.toString(16)} = ${data}`);
    videotel.send_data_to_bbs([data]);
 }
 
@@ -66,9 +71,25 @@ export function cbm_6499_read(addr: number, ticks: number)
 }
 
 // function called from C64 when CPU writes I/O in the range $DE00-DFFF
-export function cbm_6499_write(addr: number, data: number)
+export function cbm_6499_write(addr: number, data: number, ticks: number)
 {
    if(addr < 0xDF00) PIA.write(addr, data);
-   else              ACIA.cpu_write(addr, data);
+   else              ACIA.cpu_write(addr, data, ticks);
 }
 
+// funzione ring per modalità "answer"
+(window as any).ring = function() {
+   let cnt = 0;
+   let num = 13
+   function gen_int() {      
+      if(cnt < num) {
+         PIA.CRA = 0x3f | (1<<7); PIA.PA = 255;         
+         setTimeout(gen_int, 100);
+         cnt++;  
+      }
+      else {
+         setTimeout(()=>videotel.connect(), 3000);
+      }      
+   } 
+   gen_int();   
+}
